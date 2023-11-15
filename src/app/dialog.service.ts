bf0c5b3549ab353b41b6891a7823224c7fc5786b
Jorge Blanco <jorge.blanco@bilbomatica.es>
2023-11-15 22:45:48 +0100

    feat: add dialog service

---
+++ src/app/dialog.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {Dialog} from "@angular/cdk/dialog";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+
+  constructor(dialog: Dialog) {
+
+  }
+}
